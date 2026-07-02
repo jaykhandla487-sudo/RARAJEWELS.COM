307c270c305fbec7b05ac2fcf579a39e2ae8246e
jaykhandla487-sudo <jaykhandla487@gmail.com>
2026-07-02 21:12:17 +0530

    Fix sitemap

--- shop/sitemaps.py
+++ shop/sitemaps.py
@@ -9,13 +9,10 @@ class ProductSitemap(Sitemap):
     def items(self):
         return Product.objects.all()
 
-    def lastmod(self, obj):
-        return obj.updated_at
-
 
 class CategorySitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
 
     def items(self):
-        return Category.objects.filter(active=True)
+        return Category.objects.all()
