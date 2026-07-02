2e37ac6a81bcb85268e9807d167b70763f62e9f3
jaykhandla487-sudo <jaykhandla487@gmail.com>
2026-07-02 21:07:25 +0530

    Add XML sitemap

--- rarajewels/urls.py
+++ rarajewels/urls.py
@@ -10,6 +10,16 @@ from django.views.generic import RedirectView
 from django.templatetags.static import static as static_url
 from django.views.static import serve
 
+# Sitemap
+from django.contrib.sitemaps.views import sitemap
+from shop.sitemaps import ProductSitemap, CategorySitemap
+
+# Sitemap Dictionary
+sitemaps = {
+    "products": ProductSitemap,
+    "categories": CategorySitemap,
+}
+
 urlpatterns = [
     path(
         "favicon.ico",
@@ -17,6 +27,14 @@ urlpatterns = [
             url=static_url("images/favicon.ico"),
             permanent=True,
         ),
+    ),
+
+    # Sitemap
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
     ),
 
     path("admin/", admin.site.urls),
@@ -27,14 +45,14 @@ urlpatterns = [
 # Static Files
 urlpatterns += static(
     settings.STATIC_URL,
-    document_root=settings.STATIC_ROOT
+    document_root=settings.STATIC_ROOT,
 )
 
 # Media Files
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
+        document_root=settings.MEDIA_ROOT,
     )
 else:
     urlpatterns += [
